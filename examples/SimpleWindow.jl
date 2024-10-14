d7b9e5abd5546825518a3ca302b5bea5eb9271e9
SabeDoesThings <122580233+SabeDoesThings@users.noreply.github.com>
2024-10-14 13:20:48 -0500

    Update SimpleWindow.jl

--- examples/SimpleWindow.jl
+++ examples/SimpleWindow.jl
@@ -1,12 +1,24 @@
-using Nexa
+using Nexa # loads in the Nexa library
 
+# Runs only when the program started
 function on_run()
 end
 
+# Run every frame
 function update(dt::Float64)
 end
 
-function render(ctx::Nexa.Context)
+# For Rendering things to the screen
+function render(ctx::Nexa.Context) # the ctx::Nexa.Context allows to actually access the low level renderer
 end
 
-Nexa.start(on_run, update, render, "Simple Window", 1280, 720, false)
+# The Nexa.start function takes in 7 arguments
+Nexa.start(
+    on_run,           # The function ran right when the program has started
+    update,           # Called every frame
+    render,           # For rendering things to the screen
+    "Simple Window",  # The title of the window
+    1280,             # The width of the window
+    720,              # The height of the window
+    false             # wether or not the window is resiable or not
+)
